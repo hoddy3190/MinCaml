88bd610e9b21a772d9f010cc2161d97ca099583b
Hodaka Suzuki <altitude3190@gmail.com>
2021-01-11 03:38:21 +0900

    test: arrange failure messages

--- tests/lexerParser/test_lexerParser.ml
+++ tests/lexerParser/test_lexerParser.ml
@@ -1,15 +1,31 @@
 open OUnit2
+open Printf
+open Syntax
 
 let parse str =
     let lexbuf = Lexing.from_string str in
     Parser.main Lexer.token lexbuf
 
+let print_val t =
+    match t with
+    | Int i -> sprintf "\"Int %d\"" i
+    | _ -> "something but Syntax.t"
+
 let test input expected =
-    assert_equal (parse input) expected
+    (* 引数としてtest_ctxtを受けつける関数を返す。ただし今回はtest_ctxtは使わない *)
+    fun _ ->
+        assert_equal
+            expected
+            (parse input)
+            (* 改行がinputに含まれる可能性があるのでエスケープ *)
+            ~msg: (sprintf "input: %S" input)
+            ~printer: print_val
 
 let suite = "parse string" >::: [
 
-    "[int]" >:: fun _ -> test "1\n" (Int 1)
+    "[int]" >:: test
+        "1\n"
+        (Int 1);
 
 ]
 
