f1c530b2ab5c58add0b87a5a928660851b37dc13
Hodaka Suzuki <altitude3190@gmail.com>
2021-09-21 02:22:57 +0900

    docs: add comments about type variable

--- src/type.ml
+++ src/type.ml
@@ -6,6 +6,12 @@ type t = (* MinCamlの型を表現するデータ型 *)
   | Fun of t list * t (* arguments are uncurried *)
   | Tuple of t list
   | Array of t
+  (*  型変数
+      型推論で使用
+      Var(ref None)     : 未定義の型変数
+                          https://github.com/esumii/min-caml/blob/a4f74c2157de178813f712458f782688dc60e5e3/tutorial-mincaml-8.htm#L469-L473
+      Var(ref (Some t)) : 型tだと推論された型変数
+  *)
   | Var of t option ref
 
 let gentyp () = Var(ref None) (* 新しい型変数を作る *)
