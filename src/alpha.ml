5d712a1e96c94aafbcd238f8da544e9f86db15b7
Hodaka Suzuki <altitude3190@gmail.com>
2021-10-16 00:16:41 +0900

    feat: alpha.ml - Let, LetRec, App

--- src/alpha.ml
+++ src/alpha.ml
@@ -8,6 +8,10 @@ let find x env =
 (*  env: 変換前の変数名から変換後の変数名への写像
     e: k正規化後の式
     α変換後の式を返す
+    Let, LetRecで値を束縛するところでは、新しいユニークな変数を作成しenvに格納する
+    変数を使う式（Add, IfEqなど）はenvをもとに新しい変数に置き換える
+
+    xという変数が複数箇所に使われていたとき、x -> x1, x -> x2,...に変換するkey, valの組み合わせをどうやってmapに格納するのか
  *)
 let rec g env e =
   match e with
@@ -24,7 +28,25 @@ let rec g env e =
   | FDiv (s1, s2) -> FDiv (find s1 env, find s2 env)
   | IfEq (s1, s2, e1, e2) -> IfEq (find s1 env, find s2 env, g env e1, g env e2)
   | IfLE (s1, s2, e1, e2) -> IfLE (find s1 env, find s2 env, g env e1, g env e2)
-  | Let ((s1, t1), e1, e2) -> D.unimplemented "Let"
+  | Let ((s1, t1), e1, e2) ->
+    let s1' = Id.genid s1 in
+    Let((s1', t1), g env e1, g (M.add s1 s1' env) e2)
   | Var s -> Var (find s env)
-  | LetRec (fundef, e) -> D.unimplemented "LetRec"
-  | App (s, s_list) -> D.unimplemented "App"
+  | LetRec (fundef, e) ->
+    (* 実装のやり方違うけどたぶんあってる？ *)
+    let (s1, t1) = fundef.name in
+    let s1' = Id.genid s1 in
+    let updated_env_1 = M.add s1 s1' env in
+    let args' = List.map (fun s_t -> let (s, t) = s_t in (Id.genid s, t)) fundef.args in
+    let updated_env_2 = List.fold_left2 (fun acc s_t s'_t' ->
+      let s, _ = s_t in
+      let s', _ = s'_t' in
+      M.add s s' acc
+    ) updated_env_1 fundef.args args' in
+    let fundef' = {
+      name = (Id.genid s1, t1);
+      args = args';
+      body = g updated_env_2 fundef.body;
+    } in
+    LetRec (fundef', g updated_env_1 e)
+  | App (s, s_list) -> App (find s env, List.map (fun s -> find s env) s_list)
