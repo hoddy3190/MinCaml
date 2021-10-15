46c1a2661212d87fb9047903726bd5cee25d77ce
Hodaka Suzuki <altitude3190@gmail.com>
2021-10-16 00:28:10 +0900

    feat: beta.ml - Neg, Add, Sub, FNeg, FAdd, FSub, FMul, FDiv, IfEq, IfLE, Var

--- src/beta.ml
+++ src/beta.ml
@@ -1,24 +1,29 @@
 open KNormal
 
+let find x env =
+    match M.find_opt x env with
+    | Some var -> var
+    | None -> x
+
 (*  env: ある変数からそれに等しい変数への写像
     e: k正規化後の式
  *)
-let g env e =
+let rec g env e =
   match e with
   | Unit -> Unit
   | Int i -> Int i
   | Float f -> Float f
-  | Neg s -> D.unimplemented "Neg"
-  | Add (s1, s2) -> D.unimplemented "Add"
-  | Sub (s1, s2) -> D.unimplemented "Sub"
-  | FNeg s -> D.unimplemented "FNeg"
-  | FAdd (s1, s2) -> D.unimplemented "FAdd"
-  | FSub (s1, s2) -> D.unimplemented "FSub"
-  | FMul (s1, s2) -> D.unimplemented "FMul"
-  | FDiv (s1, s2) -> D.unimplemented "FDiv"
-  | IfEq (s1, s2, e1, e2) -> D.unimplemented "IfEq"
-  | IfLE (s1, s2, e1, e2) -> D.unimplemented "IfLE"
+  | Neg s -> Neg (find s env)
+  | Add (s1, s2) -> Add (find s1 env, find s2 env)
+  | Sub (s1, s2) -> Sub (find s1 env, find s2 env)
+  | FNeg s -> FNeg (find s env)
+  | FAdd (s1, s2) -> FAdd (find s1 env, find s2 env)
+  | FSub (s1, s2) -> FSub (find s1 env, find s2 env)
+  | FMul (s1, s2) -> FMul (find s1 env, find s2 env)
+  | FDiv (s1, s2) -> FDiv (find s1 env, find s2 env)
+  | IfEq (s1, s2, e1, e2) -> IfEq (find s1 env, find s2 env, g env e1, g env e2)
+  | IfLE (s1, s2, e1, e2) -> IfLE (find s1 env, find s2 env, g env e1, g env e2)
   | Let ((s1, t1), e1, e2) -> D.unimplemented "Let"
-  | Var s -> D.unimplemented "Var"
+  | Var s -> Var (find s env)
   | LetRec (fundef, e) -> D.unimplemented "LetRec"
   | App (s, s_list) -> D.unimplemented "App"
