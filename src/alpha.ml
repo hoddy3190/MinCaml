20643801019948bd6b5c7d44e4ec8799446c4528
Hodaka Suzuki <altitude3190@gmail.com>
2021-10-15 23:33:42 +0900

    feat: alpha.ml

---
+++ src/alpha.ml
@@ -0,0 +1,24 @@
+open KNormal
+
+(*  env: 変換前の変数名から変換後の変数名への写像
+    e: k正規化後の式
+ *)
+let g env e =
+  match e with
+  | Unit -> D.unimplemented "Unit"
+  | Int i -> D.unimplemented "Int"
+  | Float f -> D.unimplemented "Float"
+  | Neg s -> D.unimplemented "Neg"
+  | Add (s1, s2) -> D.unimplemented "Add"
+  | Sub (s1, s2) -> D.unimplemented "Sub"
+  | FNeg s -> D.unimplemented "FNeg"
+  | FAdd (s1, s2) -> D.unimplemented "FAdd"
+  | FSub (s1, s2) -> D.unimplemented "FSub"
+  | FMul (s1, s2) -> D.unimplemented "FMul"
+  | FDiv (s1, s2) -> D.unimplemented "FDiv"
+  | IfEq (s1, s2, e1, e2) -> D.unimplemented "IfEq"
+  | IfLE (s1, s2, e1, e2) -> D.unimplemented "IfLE"
+  | Let ((s1, t1), e1, e2) -> D.unimplemented "Let"
+  | Var s -> D.unimplemented "Var"
+  | LetRec (fundef, e) -> D.unimplemented "LetRec"
+  | App (s, s_list) -> D.unimplemented "App"
