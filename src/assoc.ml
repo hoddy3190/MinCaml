e09430e39e14d59ee7da2a70b5ff904acbb44e46
Hodaka Suzuki <altitude3190@gmail.com>
2021-10-18 23:05:06 +0900

    feat: assoc.ml

---
+++ src/assoc.ml
@@ -0,0 +1,21 @@
+open KNormal
+
+let f e =
+  match e with
+  | Unit -> D.unimplemented "Unit"
+  | Int i -> D.unimplemented "Int"
+  | Float f -> D.unimplemented "Float"
+  | Neg s -> D.unimplemented "Neg"
+  | Add (s1, s2) -> D.unimplemented "Add"
+  | Sub (s1, s2) -> D.unimplemented "Sub"
+  | FNeg s -> D.unimplemented "FNeg"
+  | FAdd (s1, s2) -> D.unimplemented "FAdd"
+  | FSub (s1, s2) -> D.unimplemented "FSub"
+  | FMul (s1, s2) -> D.unimplemented "FMul"
+  | FDiv (s1, s2) -> D.unimplemented "FDiv"
+  | IfEq (s1, s2, e1, e2) -> D.unimplemented "IfEq"
+  | IfLE (s1, s2, e1, e2) -> D.unimplemented "IfLE"
+  | Let ((s1, t1), e1, e2) -> D.unimplemented "Let"
+  | Var s -> D.unimplemented "Var"
+  | LetRec (fundef, e) -> D.unimplemented "LetRec"
+  | App (s, s_list) -> D.unimplemented "App"
