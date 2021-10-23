90e5e277fc8ee0a222123c56c6e48251028043cf
Hodaka Suzuki <altitude3190@gmail.com>
2021-10-23 23:26:58 +0900

    feat: inline.ml - LetRec

--- src/inline.ml
+++ src/inline.ml
@@ -1,4 +1,13 @@
 open KNormal
+
+(* インライン展開する関数の最大サイズ *)
+let threshold = ref 0 (* Mainで-inlineオプションによりセットされる *)
+
+let rec size = function [@warning "-4"]
+  | IfEq(_, _, e1, e2) | IfLE(_, _, e1, e2)
+  | Let(_, e1, e2) | LetRec({ body = e1 }, e2) -> 1 + size e1 + size e2
+  | _ -> 1
+
 let rec g env e =
   match e with
   | Unit -> e
@@ -16,5 +25,11 @@ let rec g env e =
   | IfLE (s1, s2, e1, e2) -> IfLE (s1, s2, g env e1, g env e2)
   | Let ((s1, t1), e1, e2) -> Let ((s1, t1), g env e1, g env e2)
   | Var _ -> e
-  | LetRec ({ name = xt; args = yts; body = e1 }, e2) -> D.unimplemented "LetRec"
+  | LetRec ({ name = (x, t); args = yts; body = e1 }, e2) ->
+    let updated_env = if size e1 <= !threshold then
+      M.add x (yts, e1) env
+    else
+      env
+    in
+    LetRec({ name = (x, t); args = yts; body = g env e1}, g env e2)
   | App _ ->  D.unimplemented "App"
