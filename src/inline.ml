cabc6140260af68acff6cbf72f7670503760d7e0
Hodaka Suzuki <altitude3190@gmail.com>
2021-10-23 23:41:15 +0900

    feat: inline.ml - App

--- src/inline.ml
+++ src/inline.ml
@@ -8,6 +8,7 @@ let rec size = function [@warning "-4"]
   | Let(_, e1, e2) | LetRec({ body = e1 }, e2) -> 1 + size e1 + size e2
   | _ -> 1
 
+(* env: keyが変数名、valが引数名のリストとLetRecの中身のペア *)
 let rec g env e =
   match e with
   | Unit -> e
@@ -32,4 +33,18 @@ let rec g env e =
       env
     in
     LetRec({ name = (x, t); args = yts; body = g env e1}, g env e2)
-  | App _ ->  D.unimplemented "App"
+  | App (s, s_list) ->
+    begin match M.find_opt s env with
+    | Some (var_list, letrec_e1) ->
+      (* letrec_e1の中の変数部分（var_list)をs_listで置き換える *)
+      let env_for_alpha = List.fold_left2
+      (fun acc (var, _) s ->
+        M.add var s acc
+      )
+      M.empty
+      var_list
+      s_list
+      in
+      Alpha.g env_for_alpha e
+    | None -> e
+    end
