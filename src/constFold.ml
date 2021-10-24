8fa8437ed635439922052b609f5d14440ab05ce9
Hodaka Suzuki <altitude3190@gmail.com>
2021-10-24 12:41:01 +0900

    feat: constFold.ml - Unit, Int, Float, Add, Sub, FAdd, FSub, FMul, FDiv

---
+++ src/constFold.ml
@@ -0,0 +1,49 @@
+open KNormal
+
+(* env : 変数の名前から値への写像
+         値は整数、浮動小数、式など
+ *)
+let rec g env e =
+  match e with
+  | Unit -> e
+  | Int _ -> e
+  | Float _ -> e
+  | Neg s -> D.unimplemented "Neg"
+  | Add (s1, s2) ->
+    begin [@warning "-4"] match M.find_opt s1 env, M.find_opt s2 env with
+    | Some (Int i1), Some (Int i2) -> Int(i1 + i2)
+    (* 少なくとも一方がNone、つまりenvに登録されていないとき、畳み込みはせずにそのまま返す *)
+    | _ -> e
+    end
+  | Sub (s1, s2) ->
+    begin [@warning "-4"] match M.find_opt s1 env, M.find_opt s2 env with
+    | Some (Int i1), Some (Int i2) -> Int(i1 - i2)
+    | _ -> e
+    end
+  | FNeg s -> D.unimplemented "FNeg"
+  | FAdd (s1, s2) ->
+    begin [@warning "-4"] match M.find_opt s1 env, M.find_opt s2 env with
+    | Some (Float f1), Some (Float f2) -> Float(f1 +. f2)
+    | _ -> e
+    end
+  | FSub (s1, s2) ->
+    begin [@warning "-4"] match M.find_opt s1 env, M.find_opt s2 env with
+    | Some (Float f1), Some (Float f2) -> Float(f1 -. f2)
+    | _ -> e
+    end
+  | FMul (s1, s2) ->
+    begin [@warning "-4"] match M.find_opt s1 env, M.find_opt s2 env with
+    | Some (Float f1), Some (Float f2) -> Float(f1 *. f2)
+    | _ -> e
+    end
+  | FDiv (s1, s2) ->
+    begin [@warning "-4"] match M.find_opt s1 env, M.find_opt s2 env with
+    | Some (Float f1), Some (Float f2) -> Float(f1 /. f2)
+    | _ -> e
+    end
+  | IfEq (s1, s2, e1, e2) -> D.unimplemented "IfEq"
+  | IfLE (s1, s2, e1, e2) -> D.unimplemented "IfLE"
+  | Let ((s1, t1), e1, e2) -> D.unimplemented "Let"
+  | Var s -> D.unimplemented "Var"
+  | LetRec ({ name = xt; args = yts; body = e1 }, e2) -> D.unimplemented "LetRec"
+  | App (s, s_list) -> D.unimplemented "App"
